50cc1984645ad270602acfba9f0854bdac50ed9c
Boxuan Wang <bw447@cam.ac.uk>
2023-07-19 15:18:15 +0800

    gpu dataset does not work...

--- audioDataset.py
+++ audioDataset.py
@@ -4,10 +4,9 @@ import torchaudio
 
 
 class AudioDataset(torch.utils.data.Dataset):
-    def __init__(self, root_dir, use_gpu = False):
+    def __init__(self, root_dir):
         self.root_dir = root_dir
         self.files = os.listdir(root_dir)
-        self.use_gpu = use_gpu
         
     def __len__(self):
         return len(self.files)
@@ -17,8 +16,6 @@ class AudioDataset(torch.utils.data.Dataset):
         file_path = os.path.join(self.root_dir, file_name)
         audio, sr = torchaudio.load(file_path)
         norm_audio = self.normalize_sr(audio, sr)
-        if self.use_gpu:
-            norm_audio = norm_audio.to('cuda')
         return norm_audio, torch.cat((norm_audio[:,1:],(torch.zeros(norm_audio.shape[0],1))),1)
         
     def normalize_sr(self, audio, sr):
