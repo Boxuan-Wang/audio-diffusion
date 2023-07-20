711732f54d014c15cc35c8736d3a723cd40ba093
Boxuan Wang <bw447@cam.ac.uk>
2023-07-20 19:01:06 +0800

    dataset chunk padding

--- audioDataset.py
+++ audioDataset.py
@@ -3,18 +3,21 @@ import torch
 import torchaudio
 import librosa
 import numpy as np
+import torch.nn.functional as F
 
 
 class AudioDataset(torch.utils.data.Dataset):
-    # split the audio into 10s chunks, less than 10s chunks are discarded
-    def __init__(self, root_dir, chunk_size = 10):
+    # split the audio into 10s chunks, less than 10s chunks are padded
+    def __init__(self, root_dir, chunk_size = 10, norm_sr = 16000):
         self.root_dir = root_dir
         self.chunk_size = chunk_size
+        self.norm_sr = norm_sr
         self.files = os.listdir(root_dir)
         _lengths = []
         for file in self.files:
             file_path = os.path.join(self.root_dir, file)
-            num_chunk = librosa.get_duration(path=file_path) // self.chunk_size
+            duration = librosa.get_duration(path=file_path)
+            num_chunk = duration // self.chunk_size + 0 if duration%self.chunk_size==0 else 1
             _lengths.append(num_chunk)
         self.lengths = np.array(_lengths)
         print("Total number of 10s chunks in dataset: ", np.sum(self.lengths))
@@ -38,12 +41,15 @@ class AudioDataset(torch.utils.data.Dataset):
         assert sr == sr_librosa
         
         norm_audio = self.normalize_sr(audio, sr)
+        if norm_audio.shape[1] < self.norm_sr * self.chunk_size:
+            # padding required
+            norm_audio = F.pad(norm_audio,(0,self.norm_sr * self.chunk_size - norm_audio.shape[1]))
         return norm_audio, torch.cat((norm_audio[:,1:],(torch.zeros(norm_audio.shape[0],1))),1)
         
     def normalize_sr(self, audio, sr):
         # Resample to 16kHz
-        if sr != 16000:
-            resampler = torchaudio.transforms.Resample(sr, 16000)
+        if sr != self.norm_sr:
+            resampler = torchaudio.transforms.Resample(sr, self.norm_sr)
             audio = resampler(audio)
         return audio
     
